dbef7568c9e921526d7f9d7c341d4ebba242f464
Amey Gurunath Parab <ameygurunathparab@gmail.com>
2023-11-20 01:07:18 -0500

    Draft 2 of fixing index.js in Database

--- Database/index.js
+++ Database/index.js
@@ -1,11 +1,11 @@
 // import courses from "./courses.json" assert { type: "json" };
 const courses = require("./courses.json");
+const modules = require("./modules.json");
+const assignments = require("./courses.json");
+const users = require("./courses.json");
+const grades = require("./courses.json");
+const enrollments = require("./courses.json");
 
-import modules from "./modules.json" assert { type: "json" };
-import assignments from "./assignments.json" assert { type: "json" };
-import users from "./users.json" assert { type: "json" };
-import grades from "./grades.json" assert { type: "json" };
-import enrollments from "./enrollments.json" assert { type: "json" };
 export default {
   courses,
   modules,
