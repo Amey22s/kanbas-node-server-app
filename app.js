2a3abd6a4ebe6984a5eb707419ec1ffad6ebef3d
Amey Gurunath Parab <ameygurunathparab@gmail.com>
2023-12-04 19:52:44 -0500

    Assignment6 changes made

--- app.js
+++ app.js
@@ -4,15 +4,45 @@ import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import UserRoutes from "./users/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
 import 'dotenv/config';
+import session from "express-session";
+import mongoose from 'mongoose';
+
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+
+console.log("Connection string for mongoDB",CONNECTION_STRING);
+mongoose.connect(CONNECTION_STRING);
+
 
 const app = express();
-app.use(cors());
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
+
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+}));
+
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
